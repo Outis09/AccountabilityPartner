1ef8f7511966a9a77b1554d23cf0360d71daa493
Samuel Ayer <ayersamuel07@gmail.com>
2025-05-19 14:28:11 +0000

    Add helper file for supabase operations

---
+++ app_streamlit/utils/supabase_client.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import streamlit as st
+from supabase import create_client, Client
+
+
+# initialize supabase client
+def init_supabase():
+    url=st.secrets["SUPABASE_URL"]
+    key=st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+# function to get user data
+# @st.cache_data
+def get_data(username):
+    """Fetch user data from the database."""
+    # get supabase client
+    supabase = st.session_state.supabase
+    if username is None:
+        return pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+    habits =  supabase.table("habits").select("*").eq("username", username).execute()
+    activities = supabase.table("activity_logs").select("*").eq("username", username).execute()
+    habits_df = pd.DataFrame(habits.data)
+    activities_df = pd.DataFrame(activities.data)
+    if habits_df.empty and activities_df.empty:
+        return habits_df, activities_df, pd.DataFrame()
+    else:
+        merged_df = pd.merge(activities_df, habits_df, on='habit_id', how='left')
+        merged_df['log_date'] = pd.to_datetime(merged_df['log_date'])
+        # merged_df.drop(columns='name_y', inplace=True)
+        # merged_df.rename(columns={'name_x':'name'}, inplace=True)
+        return habits_df, activities_df, merged_df
+    
+
+# get distinct categories
+@st.cache_data
+def get_categories(username):
+    """Fetch distinct categories from the database."""
+    # get supabase client
+    supabase = st.session_state.supabase
+    if username is None:
+        return []
+    categories = supabase.table("habits").select("category").eq("username", username).execute()
+    categories_df = pd.DataFrame(categories.data)
+    return categories_df['category'].dropna().unique().tolist()
+
+# get distinct habits for a category
+@st.cache_data 
+def get_habits(username, category):
+    """Fetch distinct habits and tracking types for a category from the database."""
+    supabase = st.session_state.supabase
+    if username is None or category is None:
+        return {}
+    habits = supabase.table("habits").select("name, tracking_type").eq("username", username).eq("category", category).execute()
+    habits_df = pd.DataFrame(habits.data)
+    if habits_df.empty:
+        return {}
+    return dict(zip(habits_df["name"], habits_df["tracking_type"]))
+
+
+# get habit id
+@st.cache_data
+def get_habit_id(username, habit_name):
+    """Fetch habit id for a habit name from the database."""
+    # get supabase client
+    supabase = st.session_state.supabase
+    if username is None or habit_name is None:
+        return None
+    habit = supabase.table("habits").select("habit_id").eq("username", username).eq("name", habit_name).execute()
+    habit_df = pd.DataFrame(habit.data)
+    if not habit_df.empty:
+        return habit_df['habit_id'].values[0]
+    else:
+        return None
+    
+# insert activity log
+def insert_activity_log(username, habit_id, log_date, activity, rating, log_notes):
+    """Insert activity log into the database."""
+    # get supabase client
+    supabase = st.session_state.supabase
+    if username is None or habit_id is None:
+        return False
+    data = {
+        "username": username,
+        "habit_id": habit_id,
+        "log_date": log_date.isoformat(),
+        "activity": activity,
+        "rating": rating,
+        "log_notes": log_notes
+    }
+    response = supabase.table("activity_logs").insert(data).execute()
+    if response.data:
+        return True
+    else:
+        return False
+    
+# insert habit
+def insert_habit(username, name, start_date, frequency, tracking_type, goal, goal_units, category, notes, end_date=None):
+    """Insert habit into the database."""
+    # get supabase client
+    supabase = st.session_state.supabase
+    if username is None:
+        return False
+    data = {
+        "username": username,
+        "name": name,
+        "start_date": start_date,
+        "frequency": frequency,
+        "tracking_type": tracking_type,
+        "goal": goal,
+        "goal_units": goal_units,
+        "category": category,
+        "notes": notes,
+        "end_date": end_date
+    }
+    response = supabase.table("habits").insert(data).execute()
+    if response.data:
+        return True
+    else:
+        return False
